3c68caa17fe9d59e2ede0463a4a55c5ece7b9107
Lukas <lukvonstrom@web.de>
2018-03-01 19:14:05 +0100

    :recycle: Updating Route Handling

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
 
-var app = express();
-
-var routes = require('./routes/rewimod');
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -13,13 +11,30 @@ app.set('view engine', 'jade');
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+// Routes
+app.get('/', getFile);
+app.get('/features', getFile);
+app.get('/download', getFile);
+app.get('/join', getFile);
+app.get('/download/:version', function (req, res) {
+    let version = req.params.version;
+    switch(version){
+        case 'latest':
+        case 'newest':
+        default:
+            return res.redirect('https://ci.rewinside.tv/job/RewiMod/lastSuccessfulBuild/');
+    }
+});
+
+function getFile(req, res){
+    return res.sendFile(path.join(__dirname, `/public/static${(req.path === "/") ? '/index': req.path }.html`));
+}
 
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
+    let err = new Error('Not Found');
+    err.status = 404;
   next(err);
 });
 
